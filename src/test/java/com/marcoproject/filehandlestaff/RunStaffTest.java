c96136809c7e505168fceaf4012a6fd74a1ac24b
Marco <marcoross37@gmail.com>
2019-02-13 22:28:53 +0200

    Adding empty unit test methods

---
+++ src/test/java/com/marcoproject/filehandlestaff/RunStaffTest.java
@@ -0,0 +1,28 @@
+package com.marcoproject.filehandlestaff;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunStaffTest {
+
+    @Test
+    public void runStaff() {
+    }
+
+    @Test
+    public void runStaffV2() {
+    }
+
+    @Test
+    public void runStaffV3() {
+    }
+
+    @Test
+    public void runStaffV4() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
