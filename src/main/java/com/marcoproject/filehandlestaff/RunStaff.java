5e79c5105280f53db920b1694c0638796e2499a1
Marco <marcoross37@gmail.com>
2019-02-13 23:08:06 +0200

    Dependancy Change from apache-io to apache-lang, and import change

--- src/main/java/com/marcoproject/filehandlestaff/RunStaff.java
+++ src/main/java/com/marcoproject/filehandlestaff/RunStaff.java
@@ -9,7 +9,7 @@ import java.util.*;
 import javax.swing.*;
 import java.io.*;
 
-import org.apache.commons.*;
+import org.apache.commons.lang.StringUtils;
 
 public class RunStaff {
     ///////////////////////Version 1///////////////////////
